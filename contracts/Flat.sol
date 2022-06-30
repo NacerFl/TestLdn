afe31d2ed8e445ff336f5b1de2b90b7fa38fac1f
AntoineMousseSetInStone <antoine.mousse@setinstone.fr>
2022-06-30 19:32:17 +0200

    test

--- contracts/Flat.sol
+++ contracts/Flat.sol
@@ -900,6 +900,207 @@ interface IJoeFactory {
     function setMigrator(address) external;
 }
 
+pragma solidity >=0.6.2;
+interface IMeerkatRouter02 {
+    function factory() external pure returns (address);
+    function WETH() external pure returns (address);
+    function swapFeeReward() external pure returns (address);
+
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountA, uint amountB, uint liquidity);
+    function addLiquidityETH(
+        address token,
+        uint amountTokenDesired,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external payable returns (uint amountToken, uint amountETH, uint liquidity);
+    function removeLiquidity(
+        address tokenA,
+        address tokenB,
+        uint liquidity,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountA, uint amountB);
+    function removeLiquidityETH(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountToken, uint amountETH);
+    function removeLiquidityWithPermit(
+        address tokenA,
+        address tokenB,
+        uint liquidity,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline,
+        bool approveMax, uint8 v, bytes32 r, bytes32 s
+    ) external returns (uint amountA, uint amountB);
+    function removeLiquidityETHWithPermit(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline,
+        bool approveMax, uint8 v, bytes32 r, bytes32 s
+    ) external returns (uint amountToken, uint amountETH);
+    function swapExactTokensForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+    function swapTokensForExactTokens(
+        uint amountOut,
+        uint amountInMax,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+    function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        payable
+        returns (uint[] memory amounts);
+    function swapTokensForExactETH(uint amountOut, uint amountInMax, address[] calldata path, address to, uint deadline)
+        external
+        returns (uint[] memory amounts);
+    function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        returns (uint[] memory amounts);
+    function swapETHForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline)
+        external
+        payable
+        returns (uint[] memory amounts);
+
+    function quote(uint amountA, uint reserveA, uint reserveB) external pure returns (uint amountB);
+    function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut, uint swapFee) external pure returns (uint amountOut);
+    function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut, uint swapFee) external pure returns (uint amountIn);
+    function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts);
+    function getAmountsIn(uint amountOut, address[] calldata path) external view returns (uint[] memory amounts);
+
+    function removeLiquidityETHSupportingFeeOnTransferTokens(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountETH);
+    function removeLiquidityETHWithPermitSupportingFeeOnTransferTokens(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline,
+        bool approveMax, uint8 v, bytes32 r, bytes32 s
+    ) external returns (uint amountETH);
+
+    function swapExactTokensForTokensSupportingFeeOnTransferTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external;
+    function swapExactETHForTokensSupportingFeeOnTransferTokens(
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external payable;
+    function swapExactTokensForETHSupportingFeeOnTransferTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external;
+}
+
+pragma solidity >=0.6.6;
+
+interface IMeerkatFactory {
+    function getPair(address tokenA, address tokenB) external view returns (address pair);
+    function createPair(address tokenA, address tokenB) external returns (address pair);
+}
+
+
+// File contracts/interfaces/IMeerkatPair.sol
+
+pragma solidity >=0.5.0;
+
+interface IMeerkatPair {
+    event Approval(address indexed owner, address indexed spender, uint value);
+    event Transfer(address indexed from, address indexed to, uint value);
+
+    function name() external pure returns (string memory);
+    function symbol() external pure returns (string memory);
+    function decimals() external pure returns (uint8);
+    function totalSupply() external view returns (uint);
+    function balanceOf(address owner) external view returns (uint);
+    function allowance(address owner, address spender) external view returns (uint);
+
+    function approve(address spender, uint value) external returns (bool);
+    function transfer(address to, uint value) external returns (bool);
+    function transferFrom(address from, address to, uint value) external returns (bool);
+
+    function DOMAIN_SEPARATOR() external view returns (bytes32);
+    function PERMIT_TYPEHASH() external pure returns (bytes32);
+    function nonces(address owner) external view returns (uint);
+
+    function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external;
+
+    event Mint(address indexed sender, uint amount0, uint amount1);
+    event Burn(address indexed sender, uint amount0, uint amount1, address indexed to);
+    event Swap(
+        address indexed sender,
+        uint amount0In,
+        uint amount1In,
+        uint amount0Out,
+        uint amount1Out,
+        address indexed to
+    );
+    event Sync(uint112 reserve0, uint112 reserve1);
+
+    function MINIMUM_LIQUIDITY() external pure returns (uint);
+    function factory() external view returns (address);
+    function token0() external view returns (address);
+    function token1() external view returns (address);
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+    function price0CumulativeLast() external view returns (uint);
+    function price1CumulativeLast() external view returns (uint);
+    function kLast() external view returns (uint);
+    function swapFee() external view returns (uint32);
+
+    function mint(address to) external returns (uint liquidity);
+    function burn(address to) external returns (uint amount0, uint amount1);
+    function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external;
+    function skim(address to) external;
+    function sync() external;
+
+    function initialize(address, address) external;
+    function setSwapFee(uint32) external;
+}
+
+
 
 /*
  * @dev Provides information about the current execution context, including the
@@ -2201,6 +2402,7 @@ contract LoadedNodesTest is ERC20, Ownable, PaymentSplitter {
     NODERewardManagement public nodeRewardManager;
 
     IJoeRouter02 public uniswapV2Router;
+    IMeerkatRouter02 public uniswapV2Router2;
 
     address public uniswapV2Pair;
     address public futurUsePool;
@@ -2261,16 +2463,16 @@ contract LoadedNodesTest is ERC20, Ownable, PaymentSplitter {
         require(futurUsePool != address(0) && distributionPool != address(0) && poolHandler != address(0), "FUTUR, REWARD & POOL ADDRESS CANNOT BE ZERO");
 
         require(uniV2Router != address(0), "ROUTER CANNOT BE ZERO");
-        
-        // IJoeRouter02 _uniswapV2Router = IJoeRouter02(uniV2Router);
 
-        // address _uniswapV2Pair = IJoeFactory(_uniswapV2Router.factory())
-        // .createPair(address(this), _uniswapV2Router.WAVAX());
+        IMeerkatRouter02 _uniswapV2Router2 = IMeerkatRouter02(uniV2Router);
 
-        // uniswapV2Router = _uniswapV2Router;
-        // uniswapV2Pair = _uniswapV2Pair;
+        address _uniswapV2Pair = IMeerkatFactory(_uniswapV2Router2.factory())
+        .createPair(address(this), _uniswapV2Router2.WETH());
 
-        // _setAutomatedMarketMakerPair(_uniswapV2Pair, true);
+        uniswapV2Router2 = _uniswapV2Router2;
+        uniswapV2Pair = _uniswapV2Pair;
+
+        _setAutomatedMarketMakerPair(_uniswapV2Pair, true);
 
         require(
             fees[0] != 0 && fees[1] != 0 && fees[2] != 0 && fees[3] != 0,
@@ -2287,13 +2489,13 @@ contract LoadedNodesTest is ERC20, Ownable, PaymentSplitter {
         require(addresses.length > 0 && balances.length > 0, "CONSTR: addresses array length must be greater than zero");
         require(addresses.length == balances.length, "CONSTR: addresses arrays length mismatch");
 
-        // for (uint256 i = 0; i < addresses.length; i++) {
-        //     _mint(addresses[i], balances[i] * (10**18));
-		// 	_isSuper[addresses[i]] = true;
-        // }
-        // require(totalSupply() == 1000000e18, "CONSTR: totalSupply must equal 1 million");
-        // require(swapAmount > 0, "CONSTR: Swap amount incorrect");
-        // swapTokensAmount = swapAmount * (10**18);
+        for (uint256 i = 0; i < addresses.length; i++) {
+            _mint(addresses[i], balances[i] * (10**18));
+			_isSuper[addresses[i]] = true;
+        }
+        require(totalSupply() == 1000000e18, "CONSTR: totalSupply must equal 1 million");
+        require(swapAmount > 0, "CONSTR: Swap amount incorrect");
+        swapTokensAmount = swapAmount * (10**18);
     }
 
 	function increaseLiquidityDistribution(uint256 amount) external onlyOwner {
--- scripts/deploy-test.js
+++ scripts/deploy-test.js
@@ -21,20 +21,6 @@ async function main() {
 
     //console.log("Node reward deployed to:", nodeReward.address);
    
-
-    const Ldn = await hre.ethers.getContractFactory("LoadedNodesTest",);
-    const ldn = await Ldn.deploy(
-    ['0xdD2FD4581271e230360230F9337D5c0430Bf44C0'], 
-    [100],
-    ['0xbDA5747bFD65F08deb54cb465eB87D40e51B197E','0x2546BcD3c84621e976D8185a91A922aE77ECEc30','0xcd3B766CCDd6AE721141F452C550Ca635964ce71','0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199'],
-    [300000,500000,100000,100000],
-    [15,40,10,20,40],
-    40,
-    '0xdF3e18d64BC6A983f673Ab319CCaE4f1a57C7097'
-    );
-
-    await ldn.deployed();
-
     const Factory = await hre.ethers.getContractFactory("MeerkatFactory",);
     const factory = await Factory.deploy('0x5362F46cabcaF813e17A6E44C4DB7286DCDf08cd');
 
@@ -46,8 +32,24 @@ async function main() {
 
     await meerkatRouter.deployed();
 
+    const Ldn = await hre.ethers.getContractFactory("LoadedNodesTest",);
+    const ldn = await Ldn.deploy(
+    ['0xdD2FD4581271e230360230F9337D5c0430Bf44C0'], 
+    [100],
+    ['0x4F30fC4E8a506BfB8e387beBA18D18Bc2d5B0e01','0x191E58fb6bD15dE0B3aB684C03fA7EE8371A5b6c','0xd0cEB79953C9175170473ada090E26259A612047','0x0A4035022Dc34fD77c0cD0bb7694120DB41f15eF'],
+    [300000,500000,100000,100000],
+    [15,40,10,20,40],
+    40,
+    meerkatRouter.address
+    );
+
+    await ldn.deployed();
+
+
+
 
     console.log("Node reward deployed to:", nodeReward.address);
+    console.log("Iterable deployed to:", iterable.address);
     console.log("Ldn deployed to:", ldn.address);
     console.log("Factory deployed to:", factory.address);
     console.log("MeerkatRouter deployed to:", meerkatRouter.address);
